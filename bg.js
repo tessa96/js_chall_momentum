f2a37dd6b68ad66dbf49a64b9dad17ec0d70d9db
tessa96 <69118609+tessa96@users.noreply.github.com>
2020-08-17 05:04:33 +0900

    Update bg.js

--- bg.js
+++ bg.js
@@ -6,7 +6,7 @@ const IMG_NUMBER = 14;
 
 function paintImage(imgNumber){
     const image = new Image();
-    image.src = url `C:/Users/JangHyerim/Desktop/nomad coder/js basic_ momentum/images/${imgNumber + 1}.jpg`;
+    image.src = `/${imgNumber + 1}.jpg`;
     image.classList.add("bgImage");
     body.prepend(image);
 }
